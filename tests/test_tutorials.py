2a01538f0b288ce660985a0b695c08585992a265
David Andrs <andrsd@gmail.com>
2022-09-11 08:22:33 -0600

    tests: run tutorials

---
+++ tests/test_tutorials.py
@@ -0,0 +1,12 @@
+import os
+from figura.__main__ import load_file
+
+tuts_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "../tutorials"
+)
+
+
+def test_bottle():
+    file = os.path.join(tuts_dir, "bottle/bottle.figura")
+    shapes = load_file(file)
