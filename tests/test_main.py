eadd228ed9883ff726917cf0cf3c4e4bfd303edb
David Andrs <andrsd@gmail.com>
2023-04-22 09:52:23 -0600

    Adding a test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import os
+import pytest
 from figura.__main__ import load_file, save_file
+from figura.__init__ import model
 
 assets_dir = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -17,3 +19,8 @@ def test_stl():
     file = os.path.join(assets_dir, "box.figura")
     shapes = load_file(file)
     save_file(shapes, "out", file_format='STL')
+
+
+def test_wrong_unit():
+    with pytest.raises(ValueError):
+        model.units = "asdf"
